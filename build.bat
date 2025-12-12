d6356b93c5b045c6cff326ee9b7790d4c5441376
luolan <luolan233@outlook.com>
2025-12-12 18:04:27 +0800

    feat(build): 优化构建脚本以确保输出目录的清理和创建

--- build.bat
+++ build.bat
@@ -7,9 +7,10 @@ set BUILD_PATH=".\build\neo-bpsys-wpf"
 set PROJ_PATH=".\neo-bpsys-wpf\neo-bpsys-wpf.csproj"
 
 :: check output dir
-if not exist %BUILD_PATH% (
-	mkdir %BUILD_PATH%
+if exist %BUILD_PATH% (
+	rmdir /s /q %BUILD_PATH%
 )
+mkdir %BUILD_PATH%
 :: build (self-contained, win-x64)
 dotnet publish %PROJ_PATH% -c Release -r win-x64 --self-contained true ^
 	/p:PublishSingleFile=false /p:PublishTrimmed=false -o %BUILD_PATH%
